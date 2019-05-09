0945bd3c7ae574412dcd57279fba511c5e23189f
bogomilmihaylov <bogomilmihaylov@live.com>
2019-05-09 06:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians,instruments) {
+  var emptyArray = [];
+function forLoop(musicians){
+  if (i === 0) {
+      instruments.push("John Lennon plays guitar"); 
+    
+  } else {
+      instruments.push(" ${i} plays guitar");
+    }
+}
+  return array;
+  
+}
+}
